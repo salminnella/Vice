2523e13348913086671d7af96b3d0859b76d100a
Anthony Minnella <anthony@Anthonys-MacBook-Pro.local>
2016-04-18 10:12:32 -0700

    article with collapsing toolbar

--- Vice/app/src/main/java/martell/com/vice/ArticleActivity.java
+++ Vice/app/src/main/java/martell/com/vice/ArticleActivity.java
@@ -1,11 +1,15 @@
 package martell.com.vice;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ArticleActivity extends AppCompatActivity {
 
@@ -15,6 +19,12 @@ public class ArticleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_article);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Top Stories");
+
+        loadBackdrop();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +35,11 @@ public class ArticleActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void loadBackdrop() {
+        final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+        Glide.with(this).load(R.drawable.pissed).centerCrop().into(imageView);
     }
 
 }
