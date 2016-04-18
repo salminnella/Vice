3a9f4870840a39a4f18be68920c449c31159683f
Anthony Minnella <anthony@Anthonys-MacBook-Pro.local>
2016-04-18 16:40:19 -0700

    adjusted gradle

--- Vice/app/src/main/java/martell/com/vice/MainActivity.java
+++ Vice/app/src/main/java/martell/com/vice/MainActivity.java
@@ -1,12 +1,9 @@
 package martell.com.vice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button articleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
