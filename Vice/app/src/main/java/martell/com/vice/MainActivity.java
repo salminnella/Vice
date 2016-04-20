27d04d71c9d392d3f8bb4cc3b14cf034c8f4b578
Anthony Minnella <anthony@Anthonys-MacBook-Pro.local>
2016-04-20 09:01:42 -0700

    cleaning up main activity

--- Vice/app/src/main/java/martell/com/vice/MainActivity.java
+++ Vice/app/src/main/java/martell/com/vice/MainActivity.java
@@ -10,12 +10,18 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
 import martell.com.vice.adapters.ViewPagerAdapter;
 import martell.com.vice.models.Article;
+import martell.com.vice.models.ArticleArray;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private ArticleAdapter articleAdapter;
     private AlphaInAnimationAdapter alphaAdapter;
     private Retrofit retrofit;
+    private Button syncButton;
 
     // Content provider authority
     public static final String AUTHORITY = "martell.com.vice.sync_adapter.StubProvider";
@@ -48,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAccount = createSyncAccount(this);
 
+        syncButton = (Button) findViewById(R.id.button_vice);
         articles = new ArrayList<>();
         retrofit = new Retrofit.Builder().baseUrl("http://www.vice.com/en_us/api/")
                 .addConverterFactory(GsonConverterFactory.create()).build();
@@ -87,6 +95,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //viewPager.setCurrentItem(1);
+        syncButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<ArticleArray> call = viceService.latestArticles(1);
+                call.enqueue(new Callback<ArticleArray>() {
+                    @Override
+                    public void onResponse(Call<ArticleArray> call, Response<ArticleArray> response) {
+                        Log.i(TAG, "onResponse: " + response.body().getData().getItems());
+//                        Article[] articleArray = response.body().getData().getItems();
+//                        ArrayList<Article> articlesNew = new ArrayList<>(Arrays.asList(articleArray));
+//                        articles.addAll(articlesNew);
+
+                    }
+                    @Override
+                    public void onFailure(Call<ArticleArray> call, Throwable t) {
+                    }
+                });
+            }
+        });
+        ContentResolver.setSyncAutomatically(mAccount, AUTHORITY, true);
         ContentResolver.addPeriodicSync(mAccount, AUTHORITY, Bundle.EMPTY, 30);
     }
 
@@ -123,60 +151,5 @@ public class MainActivity extends AppCompatActivity {
         return newAccount;
     }
 
-//    private void setupViewPagerOneFragment(final ViewPager viewPager) {
-//        adapter = new ViewPagerAdapter(getSupportFragmentManager());
-//
-//        CategoryFragment home = new CategoryFragment();
-//        Bundle bundleHome = new Bundle();
-//        bundleHome.putString(CATEGORY_TITLE_KEY, "Home");
-//        home.setArguments(bundleHome);
-//
-//        CategoryFragment news = new CategoryFragment();
-//        Bundle bundleNews = new Bundle();
-//        bundleNews.putString(CATEGORY_TITLE_KEY, "News");
-//        news.setArguments(bundleNews);
-//
-//        CategoryFragment music = new CategoryFragment();
-//        Bundle bundleMusic = new Bundle();
-//        bundleMusic.putString(CATEGORY_TITLE_KEY, "Music");
-//        music.setArguments(bundleMusic);
-//
-//        CategoryFragment music = new CategoryFragment("Music");
-//        CategoryFragment sports = new CategoryFragment("Sports");
-//        CategoryFragment tech = new CategoryFragment("Tech");
-//        CategoryFragment travel = new CategoryFragment("Travel");
-//        CategoryFragment fashion = new CategoryFragment("Fashion");
-//        CategoryFragment guide = new CategoryFragment("Guide");
-//
-//        adapter.addFragment(home, bundleHome.getString(CATEGORY_TITLE_KEY));
-//        adapter.addFragment(news, bundleNews.getString(CATEGORY_TITLE_KEY));
-//        adapter.addFragment(music, bundleMusic.getString(CATEGORY_TITLE_KEY));
-//        adapter.addFragment(sports, sports.getTitle());
-//        adapter.addFragment(tech, tech.getTitle());
-//        adapter.addFragment(travel, travel.getTitle());
-//        adapter.addFragment(fashion, fashion.getTitle());
-//        adapter.addFragment(guide, guide.getTitle());
-//
-//        adapter.notifyDataSetChanged();
-//
-//        viewPager.setAdapter(adapter);
-//
-//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                Toast.makeText(MainActivity.this, position + "", Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        });
-//    }
 
 }
