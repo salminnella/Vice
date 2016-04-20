7a2f1d7f9f319ec05a03568013800316f8dbb5ce
Anthony Minnella <anthony@Anthonys-MacBook-Pro.local>
2016-04-19 22:47:54 -0700

    updated manifest for stubprovider, added SyncAdapter class, and syncadapter xml

---
+++ Vice/app/src/main/java/martell/com/vice/sync_adapter/SyncAdapter.java
@@ -0,0 +1,70 @@
+package martell.com.vice.sync_adapter;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+/**
+ * Created by anthony on 4/19/16.
+ *
+ */
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+    // Global variables
+    // Define a variable to contain a content resolver instance
+    ContentResolver mContentResolver;
+
+    /**
+     * Set up the sync adapter
+     */
+    public SyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+    }
+
+
+
+    /**
+     * Set up the sync adapter. This form of the
+     * constructor maintains compatibility with Android 3.0
+     * and later platform versions
+     */
+    public SyncAdapter(
+            Context context,
+            boolean autoInitialize,
+            boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+
+    }
+
+    /**
+     * This is where the magic background sync actually happens
+     * @param account
+     * @param extras
+     * @param authority
+     * @param provider
+     * @param syncResult
+     */
+    @Override
+    public void onPerformSync(
+            Account account,
+            Bundle extras,
+            String authority,
+            ContentProviderClient provider,
+            SyncResult syncResult) {
+
+    }
+}
