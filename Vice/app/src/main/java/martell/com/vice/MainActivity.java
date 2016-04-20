3a1049ab95f6d65d89ec255c7d2e34844e58b32f
Anthony Minnella <anthony@Anthonys-MacBook-Pro.local>
2016-04-19 23:11:48 -0700

    added authority, account_type, and account to main. also creates dummy account for sync adapter

--- Vice/app/src/main/java/martell/com/vice/MainActivity.java
+++ Vice/app/src/main/java/martell/com/vice/MainActivity.java
@@ -1,5 +1,8 @@
 package martell.com.vice;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -29,10 +32,20 @@ public class MainActivity extends AppCompatActivity {
     private AlphaInAnimationAdapter alphaAdapter;
     private Retrofit retrofit;
 
+    // Content provider authority
+    public static final String AUTHORITY = "martell.com.vice.sync_adapter.StubProvider";
+    // Account type
+    public static final String ACCOUNT_TYPE = "example.com";
+    // Account
+    public static final String ACCOUNT = "default_account";
+    Account mAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAccount = createSyncAccount(this);
 
         articles = new ArrayList<>();
         retrofit = new Retrofit.Builder().baseUrl("http://www.vice.com/en_us/api/")
@@ -74,6 +87,39 @@ public class MainActivity extends AppCompatActivity {
 
         //viewPager.setCurrentItem(1);
 
+    }
+
+    /**
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account createSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+          /*
+           * If you don't set android:syncable="true" in
+           * in your <provider> element in the manifest,
+           * then call context.setIsSyncable(account, AUTHORITY, 1)
+           * here.
+           */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+        return newAccount;
     }
 
 //    private void setupViewPagerOneFragment(final ViewPager viewPager) {
