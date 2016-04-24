f9ff0ff4e8072161c24946e7be1718a9343e290a
Anthony Minnella <anthony@Anthonys-MacBook-Pro.local>
2016-04-24 16:59:53 -0700

    little refactoring in article adapter, needs more javadocs

--- Vice/app/src/main/java/martell/com/vice/ArticleAdapter.java
+++ Vice/app/src/main/java/martell/com/vice/ArticleAdapter.java
@@ -3,14 +3,12 @@ package martell.com.vice;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -21,13 +19,13 @@ import martell.com.vice.models.Article;
  * Created by adao1 on 4/18/2016.
  */
 public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
-    private static final String TAG = "ArticleAdapter";
 
+    // region Member Variables
     private List<Article> mArticles;
-    private ImageLoaderConfiguration config;
     private final OnRVItemClickListener listener;
     private final OnLastArticleShownListener lastArticleShownListener;
     private Context context;
+    // endregion Member Variables
 
     public interface OnRVItemClickListener {
         void onRVItemClick(Article article);
@@ -36,6 +34,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         void onLastArticleShown(int position);
     }
 
+    // constructor
     public ArticleAdapter(List<Article> mArticles, OnRVItemClickListener listener, OnLastArticleShownListener lastArticleShownListener) {
         this.mArticles = mArticles;
         this.listener = listener;
@@ -46,11 +45,8 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
     public ArticleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
-        config = new ImageLoaderConfiguration.Builder(parent.getContext()).build();
         View articleView = inflater.inflate(R.layout.recycler_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(articleView);
-        Log.d(TAG, "onCreateViewHolder: ");
-        return viewHolder;
+        return new ViewHolder(articleView);
 
     }
 
@@ -64,9 +60,6 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         }
         ImageView imageView = holder.imageView;
         titleTextView.setText(article.getArticleTitle());
-//        ImageLoader imageLoader = ImageLoader.getInstance(); // Get singleton instance
-//        imageLoader.init(config);
-//        imageLoader.displayImage(article.getArticleThumbURL(), imageView);
         Picasso.with(context)
                 .load(article.getArticleThumbURL())
                 .into(imageView);
@@ -75,7 +68,6 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         if (position== mArticles.size()-1){
             lastArticleShownListener.onLastArticleShown(position);
         }
-
     }
 
     @Override
@@ -95,7 +87,6 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
             this.imageView = (ImageView)itemView.findViewById(R.id.item_image);
         }
         public void bind(final Article article, final OnRVItemClickListener listener){
-            //titleTextView.setText(article.getArticleTitle());
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
