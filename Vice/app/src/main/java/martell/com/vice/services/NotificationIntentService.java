a9f149f958f38c1085acccb7fe5cffc82a1d1c1b
Anthony Minnella <anthony@Anthonys-MacBook-Pro.local>
2016-04-26 13:32:36 -0700

    fixing notifications

--- Vice/app/src/main/java/martell/com/vice/services/NotificationIntentService.java
+++ Vice/app/src/main/java/martell/com/vice/services/NotificationIntentService.java
@@ -1,5 +1,4 @@
 package martell.com.vice.services;
-
 import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -7,7 +6,7 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
-
+import android.util.Log;
 import martell.com.vice.activity.ArticleActivity;
 import martell.com.vice.activity.MainActivity;
 import martell.com.vice.R;
@@ -17,18 +16,17 @@ import martell.com.vice.R;
  */
 public class NotificationIntentService extends IntentService{
 
-    // region Constants
+    private static final String TAG = "NotificationService";
     public static final int NOTIFICATION_ID = 5453;
+    public String articleIDExtra;
     public static final String EXTRA_ID = "ID_KEY";
     public static final String EXTRA_TITLE = "TITLE_KEY";
     public static final String EXTRA_PREVIEW = "PREVIEW_KEY";
-    public static final String ARTICLE_ID_KEY = "ID_KEY";
-    public static final String VICE_NEWS_TITLE = "Vice News";
-    public static final String NOTIFICATION_INTENT_SERVICE = "NotificationIntentService";
-    // endregion Constants
+    private String articleID;
+    private String articleTitle;
 
     public NotificationIntentService() {
-        super(NOTIFICATION_INTENT_SERVICE);
+        super("NotificationIntentService");
     }
 
     @Override
@@ -41,15 +39,19 @@ public class NotificationIntentService extends IntentService{
             }
         }
 
-        String articleID = intent.getStringExtra(EXTRA_ID);
-        String articleTitle = intent.getStringExtra(EXTRA_TITLE);
+        articleID = intent.getStringExtra(EXTRA_ID);
+        articleTitle = intent.getStringExtra(EXTRA_TITLE);
+        Log.i(TAG, "Title: " + articleTitle);
+        Log.i(TAG, "id: " + articleID);
+
         showArticleTitle(articleID, articleTitle);
     }
 
     private void showArticleTitle(String id, String title) {
 
         Intent intent = new Intent(this, ArticleActivity.class);
-        intent.putExtra(ARTICLE_ID_KEY, id);
+        intent.putExtra("ID_KEY", id);
+        Log.i(TAG, "you clicked article id: " + id);
 
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         stackBuilder.addParentStack(MainActivity.class);
@@ -60,7 +62,7 @@ public class NotificationIntentService extends IntentService{
 
         Notification notification = new Notification.Builder(this)
                 .setSmallIcon(R.mipmap.ic_notification)
-                .setContentTitle(VICE_NEWS_TITLE)
+                .setContentTitle("Vice News")
                 .setAutoCancel(true)
                 .setPriority(Notification.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
@@ -70,5 +72,7 @@ public class NotificationIntentService extends IntentService{
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, notification);
+
     }
+
 }
